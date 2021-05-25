0f78be77789bd4d3e7453391bb87db4361ace6d1
D-Bhatta <dbhatta1232@gmail.com>
2021-05-25 12:53:24 -0400

    ref(view): Create method to return auth url
    Create a method that will use the `msal.ConfidentialClientApplication`
    client to return an authorization request url, which will then redirect
    to the url for "authorized".

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -146,5 +146,11 @@ def _build_msal_app(cache=None, authority=None):
 
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return None
+    client = _build_msal_app(cache=_load_cache(), authority=Config.AUTHORITY)
+    auth_url = client.get_authorization_request_url(
+        scopes=scopes or [],
+        state=state or str(uuid.uuid4()),
+        redirect_uri=url_for("authorized", _external=True, _scheme="https"),
+    )
+
+    return auth_url
