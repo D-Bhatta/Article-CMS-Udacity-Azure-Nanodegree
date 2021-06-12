f9d6eef3c75745e3ec4ec1b193974b034276d035
D-Bhatta <dbhatta1232@gmail.com>
2021-06-12 10:30:04 -0400

    ref(view): Add logging for valid login attempt
    Add logging via app logger for valid login attempt.

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -73,6 +73,7 @@ def login():
             app.logger.info("Authentication error: Invalid login attempt")
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
+        app.logger.info("Successful login by 'admin' account.")
         next_page = request.args.get("next")
         if not next_page or url_parse(next_page).netloc != "":
             next_page = url_for("home")
